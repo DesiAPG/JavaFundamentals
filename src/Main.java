0616a9661acb7ca7142cd6565e2af3b94e820e3d
DesiAPG <desireapgames@gmail.com>
2022-06-07 22:20:37 -0500

    Data Types

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        /*
+        Data types
+         */
+
+        // Numbers
+        byte number1 = 1;
+        int number2 = 12;
+        float number3 = 3.4f;
+        double number4 = 3.12d;
+
+        // Character
+        char character = 'a';
+
+        // Booleans
+        boolean bool = true;
+        boolean bool1 = false;
+
+
+        // Text
+
+        String text = "Hello world";
+
+        // Envoltorio
+
+        Integer int1 = null;
+
+
+    }
+}
