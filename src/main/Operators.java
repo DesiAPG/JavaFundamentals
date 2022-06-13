7da2b3362e9d5690ca4884d581e48d9129e2f0b9
DesiAPG <desireapgames@gmail.com>
2022-06-13 10:08:04 -0500

    Class 6 - OOP

---
+++ src/main/Operators.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Operators {
+
+
+    public static void main(String[] args) {
+        int value1 = 412;
+        int value2 = 12;
+        System.out.println(value1 + value2);
+    }
+}
---
+++ src/test/Main.java
@@ -0,0 +1,9 @@
+package test;
+
+import main.Functions;
+
+public class Main {
+    public static void main(String[] args) {
+        Functions.HolaMundo("juan");
+    }
+}
