3471a93d2a67052a0fcd0e52f645664114ce1b18
DesiAPG <desireapgames@gmail.com>
2022-06-13 10:07:35 -0500

    Functions and objects

---
+++ src/main/Functions.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Functions {
+    public static void main(String[] args) {
+        HolaMundo("Pedro");
+    }
+
+    public static void HolaMundo(String name) {
+        System.out.println("Hola! " + name);
+    }
+}
